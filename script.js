b048f5b9a44c13e6df9c7b5fcc1dd6eb029df551
Ghidyon <gideonakunana@gmail.com>
2020-11-15 00:47:05 +0100

    second commit

--- script.js
+++ script.js
@@ -5,6 +5,23 @@ function getCountries() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
+            let output = `<h2>Countries</h2>`;
+            data.forEach(country => {
+                const { name, capital, region, timezones, currencies, flag } = country;
+                output += `<div class="country">
+                            <ul>
+                                <li>${name}</li>
+                                <li>Capital: ${capital}</li>
+                                <li>Region: ${region}</li>
+                                <li>Time Zone(s): ${timezones}</li>
+                                <li>Currency: ${currencies[0].name}, ${currencies[0].symbol}</li>
+                            </ul>
+                            <div>
+                                <img src="${flag} title="${name}">
+                            </div>
+                          </div>`;
+            })
+            document.querySelector('.countries').innerHTML = output;
         })
         .catch(() => {
             const errorMessage = `<img src="images/icon-error.svg">
