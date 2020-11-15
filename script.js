3790835b1a9db1277a50679da6949b244257ffa0
Ghidyon <gideonakunana@gmail.com>
2020-11-15 07:03:36 +0100

    fourth commit

--- script.js
+++ script.js
@@ -5,22 +5,24 @@ function getCountries() {
         .then(response => response.json())
         .then(data => {
             console.log(data);
-            let output = `<h2>Countries</h2>`;
+            let header = `<h2>Countries</h2>`;
+            let output = ``;
             data.forEach(country => {
                 const { name, capital, region, timezones, currencies, flag } = country;
                 output += `<div class="country">
                             <ul>
-                                <li>${name}</li>
+                                <li><strong>${name}</strong></li>
                                 <li>Capital: ${capital}</li>
                                 <li>Region: ${region}</li>
-                                <li>Time zone(s): ${timezones[0]}</li>
+                                <li>Time zone: ${timezones[0]}</li>
                                 <li>Currency: ${currencies[0].name}, ${currencies[0].symbol}</li>
                             </ul>
                             <div>
-                                <img src="${flag}" title="${name}">
+                                <img class="flag" src="${flag}" title="${name}">
                             </div>
                           </div>`;
             })
+            document.querySelector('.header').innerHTML = header;
             document.querySelector('.countries').innerHTML = output;
         })
         .catch(() => {
